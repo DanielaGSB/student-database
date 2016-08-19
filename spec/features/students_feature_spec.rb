baed014456d0e0e24c5c723692698f3e774e0ece
DanielaGSB <Daniela.galeote@gmail.com>
2016-08-19 10:27:26 +0100

    Passing feature test for no students to display, should prompt user to add a student

---
+++ spec/features/students_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'students' do
+  context 'no students have been added' do
+    scenario 'should display a prompt to add a student' do
+      visit '/'
+      expect(page).to have_content 'No students to show'
+      expect(page).to have_link 'Add a student'
+    end
+  end
+end
