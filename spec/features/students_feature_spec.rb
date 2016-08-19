a172a341ca00091c45b20912fe967384dc310c0f
DanielaGSB <Daniela.galeote@gmail.com>
2016-08-19 10:46:01 +0100

    Passing feature test for students displayed when added to database

--- spec/features/students_feature_spec.rb
+++ spec/features/students_feature_spec.rb
@@ -8,4 +8,15 @@ feature 'students' do
       expect(page).to have_link 'Add a student'
     end
   end
+
+  context 'students have been added' do
+    before do
+      Student.create(name: 'Luca', email: 'luca@gmail.com')
+    end
+    scenario 'display students' do
+      visit '/'
+      expect(page).to have_content 'Luca'
+      expect(page).not_to have_content 'No students to show'
+    end
+  end
 end
